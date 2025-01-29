ae979b74622cdf1d4fb83c59bf1d69b43dd35261
womimc <138317871+womimc@users.noreply.github.com>
2025-01-29 12:52:24 +0100

    Update VIRUS.bat

--- VIRUS.bat
+++ VIRUS.bat
@@ -8,7 +8,7 @@ echo WARNING!
 echo THIS VIRUS OVERLOADS YOUR PC, DO YOU WANT TO RUN IT?
 echo
 echo You can use other device to remove virus there: C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
-eco.
+echo.
 echo.
 set /p continue="[YES / NO] >> "
 if /i "%continue%"=="yes" goto run
